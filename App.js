c064facb2c5e33a3ff15b4b79bffcf8d8206595f
Victor Robson Botto Goulart <vectro2017@gmail.com>
2024-05-25 11:10:44 -0300

    Update App.JS
    Update

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import React from 'react';
+import Slider from '@react-native-comunity/slider'
 
 export default function App(){
   return(
+
+    //START - IN
     <View style={styles.container}>
-      <Image
-        source={require("./src/assets/logosf.png")}
-        style={styles.logo}
-      />
-      <Text Style={styles.title}>F.O.D.A</Text>
+
+        <Image
+            source={require("./src/assets/logosf.png")}
+            style={styles.logo}
+        />
+        <Text Style={styles.title}>F.O.D.A</Text>
+
+        <View style={styles.area}>
+            <Slider
+            style={{height: 50}}
+            minimumValue={6}
+            maximumValue={20}
+            
+            />
+        </View>
 
     </View>
+
+    //END - OUT
   )
 }
 
+//Background color and center layout
 const styles = StyleSheet.create({
   container:{
     flex: 1,
@@ -21,10 +37,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  logo: {
-    marginBottom: 60,
-    width: 200, // Defina a largura da imagem conforme necessário
-    height: 200, // Defina a altura da imagem conforme necessário
-    resizeMode: 'contain' // Ajuste a imagem dentro do contêiner sem distorção
-  }
+    logo: 
+    {
+
+        //center logo image
+        marginBottom: 60,
+        width: 200, // Defina a largura da imagem conforme necessário
+        height: 200, // Defina a altura da imagem conforme necessário
+        resizeMode: 'contain' // Ajuste a imagem dentro do contêiner sem distorção
+    },
+    area: 
+    {
+        marginTop: 14,
+        margimBottom: 14,
+        width: "80%",
+        backgroundColor: 'red'    
+    },
 })
